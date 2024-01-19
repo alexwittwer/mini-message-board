1b3fd6b8ef6da4083ae62bcb0c351ec9d2dc7f7f
alexwittwer <wittweral@gmail.com>
2024-01-18 20:54:32 -0500

    add anonymous as default

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
-  user: { type: String, minLength: 1, maxLength: 25 },
+  user: { type: String, minLength: 1, maxLength: 25, default: 'anonymous'},
   text: { type: String, minLength: 1, maxLength: 800 },
   added: { type: String, default: DateTime.now().setZone("America/New_York").toLocaleString(DateTime.DATETIME_MED) },
 });

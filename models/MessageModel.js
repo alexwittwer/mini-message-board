0bf1136e0009eba5d5b9c0ece47b96624a9ee83c
alexwittwer <wittweral@gmail.com>
2024-01-18 20:53:21 -0500

    add luxon for time stamping

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
-    user: {type: String, minLength: 1, maxLength: 25},
-    text: {type: String, minLength: 1, maxLength: 800},
-    added: {type: Date}
-})
+  user: { type: String, minLength: 1, maxLength: 25 },
+  text: { type: String, minLength: 1, maxLength: 800 },
+  added: { type: String, default: DateTime.now().setZone("America/New_York").toLocaleString(DateTime.DATETIME_MED) },
+});
 
 module.exports = mongoose.model("Message", MessageSchema);

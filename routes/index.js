bf9014b3b5007c70b1fd606199bf19abf527dccd
alexwittwer <wittweral@gmail.com>
2024-01-18 20:42:21 -0500

    update styling

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const message_controller = require('../controllers/messageController')
-const new_controller = require('../controllers/addMessageController')
+const message_controller = require("../controllers/messageController");
+const new_controller = require("../controllers/addMessageController");
+const { query, validationResult } = require("express-validator");
 
 /* GET home page. */
-router.get("/", message_controller.messages)
+router.get("/", message_controller.messages);
 
 router.get("/new", (req, res, next) => {
   res.render("form");
 });
 
-router.post("/new", new_controller.new_messages )
+router.post("/new", new_controller.new_messages);
 
 module.exports = router;

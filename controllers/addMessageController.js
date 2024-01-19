91ae67e433cbad5de509879a9f3f8bcf1d9b2f37
alexwittwer <wittweral@gmail.com>
2024-01-18 20:58:27 -0500

    fix date changing, add anonymous for no name

--- controllers/addMessageController.js
+++ controllers/addMessageController.js
@@ -1,8 +1,15 @@
 const Message = require("../models/MessageModel");
 const asyncHandler = require("express-async-handler");
+const { DateTime } = require("luxon");
 
 // Add a new message to database
 exports.new_messages = asyncHandler(async (req, res, next) => {
+  if (!req.body.user){
+    req.body.user = 'Anonymous'
+  }
+  req.body.date = DateTime.now()
+    .setZone("America/New_York")
+    .toLocaleString(DateTime.DATETIME_MED);
   const newMessage = await Message.create(req.body);
 
   res.redirect("/");
--- models/MessageModel.js
+++ models/MessageModel.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
-  user: { type: String, minLength: 1, maxLength: 25, default: 'anonymous'},
+  user: { type: String, maxLength: 25, default: "Anonymous" },
   text: { type: String, minLength: 1, maxLength: 800 },
-  added: { type: String, default: DateTime.now().setZone("America/New_York").toLocaleString(DateTime.DATETIME_MED) },
+  added: { type: String },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
